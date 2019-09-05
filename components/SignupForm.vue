19efaf5cea02427ef0b1b58fd23c12c5a5555b24
Kobayashi Shunta <monta.k0523@gmail.com>
2019-09-05 18:23:35 +0900

    signup design

--- components/SignupForm.vue
+++ components/SignupForm.vue
@@ -1,20 +1,23 @@
 <template>
-  <div>
-    <form>
-      <div class="form-group">
+  <div class="signup-card">
+    <form class="md-w50">
+      <div class="form-group text-left">
         <label for="name">Name</label>
         <input type="text" class="form-control" id="name" v-model="name">
       </div>
-      <div class="form-group">
+      <div class="form-group text-left">
         <label for="email">Email</label>
         <input type="email" class="form-control" id="email" v-model="email">
       </div>
-      <div class="form-group">
+      <div class="form-group text-left">
         <label for="password">Password</label>
         <input type="password" class="form-control" id="password" v-model="password">
       </div>
     </form>
-    <button class="btn btn-dark" @click="signup()">ユーザー登録する</button>
+    <button class="btn btn-dark" @click="signup()">
+      <font-awesome-icon :icon="['fas','check']" />
+      ユーザー登録する
+    </button>
   </div>
 </template>
 
@@ -50,3 +53,20 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.signup-card {
+  padding: 30px 10px;
+  border: solid 0.2px gray;
+  border-radius: 10px;
+  background-color: #eee;
+  margin-top: 100px;
+}
+
+@media screen and (min-width: 767px) {
+  .md-w50 {
+    width: 50%;
+    margin: auto;
+  }
+}
+</style>
