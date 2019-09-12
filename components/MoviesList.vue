f29beb5613d9a2ccc168113268b51d6d1dff035c
Kobayashi Shunta <monta.k0523@gmail.com>
2019-09-12 21:54:52 +0900

    上映時間が未定の作品の表示非表示

--- components/MoviesList.vue
+++ components/MoviesList.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="row">
-    <movie-content class="col-md-4 col-12 mt-2" v-for="(movie, index) in movies" :key="index" :movie="movie"></movie-content>
+    <div class="col-12">
+      <input type="checkbox" value="true" id="lengthToggle" v-model="hideUndecidedLength">
+      <label for="lengthToggle">
+        上映時間未定を隠す
+      </label>
+    </div>
+    <movie-content class="col-md-4 col-12 mt-2" v-for="(movie, index) in narrowedMovies" :key="index" :movie="movie"></movie-content>
   </div>
 </template>
 
@@ -9,7 +15,19 @@ import { mapGetters } from 'vuex'
 import MovieContent from '~/components/MovieContent'
 
 export default {
+  data() {
+    return {
+      hideUndecidedLength: false
+    }
+  },
   computed: {
+    narrowedMovies() {
+      if (this.hideUndecidedLength) {
+        return this.movies.filter(movie => movie.length !== 0)
+      } else {
+        return this.movies
+      }
+    },
     ...mapGetters(['movies'])
   },
   components: {
